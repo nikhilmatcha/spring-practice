81ece7b8606b97d67b5cfa2ead18d509f5fb1101
nikhilmatcha <109213401+nikhilmatcha@users.noreply.github.com>
2022-07-14 03:12:30 -0700

    Add files via upload

--- springDemo/src/main/java/com/example/springDemo/SpringDemoApplication.java
+++ springDemo/src/main/java/com/example/springDemo/SpringDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.springDemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringDemoApplication {
-
+	@Value("${spring.application.name}")
+	private String name;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 	@RequestMapping(value="/")
 	public String func(){
-		return "Haii!! I am Nikhil Matcha." + "\r\n" + "My employee id: 3410";	
+		return "Haii!! I am Nikhil Matcha. My employee id: 3410. The application name: " + name;	
 	}
 
 }
