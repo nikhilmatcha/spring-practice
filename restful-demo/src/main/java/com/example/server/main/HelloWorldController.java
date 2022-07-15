d5f24b450f19648bb3a292a3cc8c315127ef125f
nikhilmatcha <109213401+nikhilmatcha@users.noreply.github.com>
2022-07-15 06:09:59 -0700

    Add files via upload

--- restful-demo/src/main/java/com/example/server/main/HelloWorldController.java
+++ restful-demo/src/main/java/com/example/server/main/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.server.main;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -16,6 +17,11 @@ public class HelloWorldController {
 	@GetMapping(path="/bean")
 	public HelloWorldBean helloBean() {
 		return  new HelloWorldBean("Hello World.");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")  
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {  
+		return new HelloWorldBean(String.format("Hello World, %s", name)); //%s replace the name  
 	}
 	
 }
