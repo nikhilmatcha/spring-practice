621ce7be04b409f5cefe3b690d4d7e939a88bcc1
nikhilmatcha <109213401+nikhilmatcha@users.noreply.github.com>
2022-07-17 11:36:21 -0700

    Add files via upload

---
+++ secureWebApplicationdemo/src/main/java/com/example/secureWebApplicationdemo/SecureWebApplicationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureWebApplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebApplicationdemoApplication {
+
+	public static void main(String[] args) throws Throwable{
+		SpringApplication.run(SecureWebApplicationdemoApplication.class, args);
+	}
+
+}
